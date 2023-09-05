424c1adff79379c469201bbce2c65a7ba6852623
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-09-05 17:52:31 +0100

    feat(loading.tsx): Added a loading spinner

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Loading() {
-  return <div>loading</div>;
+  return (
+    <div className="flex h-[50vh] w-full items-center justify-center">
+      <div className="h-20 w-20 animate-spin rounded-[50%] border-[0.625rem] border-solid border-white border-t-[#69A8D4]"></div>
+      <p className="text-2xl font-black text-textColor">Loading</p>
+    </div>
+  );
 }
 
 export default Loading;
