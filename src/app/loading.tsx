920bc978c7cabe80c3c454b83daf86decbe6dda4
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-09-05 17:54:23 +0100

    style(loading.tsx): Changed the flex direction

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Loading() {
   return (
-    <div className="flex h-[50vh] w-full items-center justify-center">
+    <div className="flex h-[50vh] w-full flex-col items-center justify-center">
       <div className="h-20 w-20 animate-spin rounded-[50%] border-[0.625rem] border-solid border-white border-t-[#69A8D4]"></div>
       <p className="text-2xl font-black text-textColor">Loading</p>
     </div>
