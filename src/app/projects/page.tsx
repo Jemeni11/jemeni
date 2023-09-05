16e2729b63fb9cee250df84a1eec2f32f2e5b2b5
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-09-05 16:22:07 +0100

    feat(projects/page.tsx): Improved SEO

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,16 +2,27 @@ import Image from "next/image";
 import Link from "next/link";
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Project | Emmanuel Jemeni",
-  metadataBase: new URL("https://jemeni.vercel.app/projects"),
+const meta = {
+  title: "Projects | Emmanuel Jemeni",
+  url: "https://jemeni.vercel.app/projects",
   description: "Explore projects built by Emmanuel Jemeni",
+  image: "https://res.cloudinary.com/dro4rspo2/image/upload/v1693924615/OG-Image-Projects_bl69fp.png",
+};
+
+export const metadata: Metadata = {
+  title: "Projects",
+  description: meta.description,
   openGraph: {
-    title: "Projects | Emmanuel Jemeni",
-    url: "https://jemeni.vercel.app/projects",
-    description: "Explore projects built by Emmanuel Jemeni",
-    // images:
-    // "https://res.cloudinary.com/victoreke/image/upload/v1692636087/victoreke/projects.png",
+    title: meta.title,
+    url: meta.url,
+    description: meta.description,
+    images: meta.image,
+  },
+  twitter: {
+    title: meta.title,
+    site: meta.url,
+    description: meta.description,
+    images: [meta.image],
   },
 };
 
