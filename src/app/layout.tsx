53bd4d3b278eddeb44be570da31b48fb605fa517
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-09-05 16:19:10 +0100

    feat[layout.tsx, page.tsx]: Updated Metadata and removed template code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,37 +3,55 @@ import type { Metadata } from "next";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Inter } from "next/font/google";
-import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+const meta = {
+  image: "https://res.cloudinary.com/dro4rspo2/image/upload/v1693898610/OG-Image_jianeb.png",
+  url: "https://jemeni.vercel.app",
+  name: "Emmanuel Jemeni",
   title: "Emmanuel Jemeni | Front-end Developer",
-  metadataBase: new URL("https://jemeni.vercel.app"),
-  description:
-    "Emmanuel Jemeni is a Front-end developer focused on building responsive websites for amazing experiences on the web.",
+  description: "Emmanuel Jemeni is a Front-end developer focused on building responsive websites for amazing experiences on the web.",
+};
+
+export const metadata: Metadata = {
+  title: {
+    default: meta.title,
+    template: "%s | Emmanuel Jemeni",
+  },
+  keywords: [meta.name, "Portfolio", "Front-end Developer"],
+  creator: meta.name,
+  metadataBase: new URL(meta.url),
+  description: meta.description,
   openGraph: {
-    title: "Emmanuel Jemeni | Front-end Developer",
-    url: "https://jemeni.vercel.app",
+    title: meta.title,
+    url: meta.url,
     siteName: "vercel.app",
     locale: "en-US",
     type: "website",
-    description:
-      "Emmanuel Jemeni is a Front-end developer focused on building responsive websites for amazing experiences on the web.",
-    images:
-      "https://res.cloudinary.com/dro4rspo2/image/upload/v1693898610/OG-Image_jianeb.png",
+    description: meta.description,
+    images: meta.image,
+  },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    userScalable: false,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: meta.title,
+    site: meta.url,
+    description: meta.description,
+    creator: "@Jemeni11",
+    images: [meta.image],
   },
   alternates: {
-    canonical: "https://jemeni.vercel.app",
+    canonical: meta.url,
   },
   themeColor: "#142538",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
